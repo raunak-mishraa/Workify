265ae71db7997d5c4ce32cd36065322b0a15cb3e
raunak mishra <raunakmishra6301@gmail.com>
2024-03-30 00:22:52 +0530

    added message

--- src/components/RTC/Messages.jsx
+++ src/components/RTC/Messages.jsx
@@ -2,10 +2,10 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import moment from "moment";
+import moment from "moment";
 import newRequest from "../../assets/utils/newRequest";
 import Container from "../container/Container";
-
+import Button from "../Button";
 const Messages = () => {
   const currentUser = JSON.parse(localStorage.getItem("userData"));
   const queryClient = useQueryClient();
@@ -18,34 +18,34 @@ const Messages = () => {
       }),
   });
 
-  // const mutation = useMutation({
-  //   mutationFn: (id) => {
-  //     return newRequest.put(`/conversations/${id}`);
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries(["conversations"]);
-  //   },
-  // });
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.put(`/conversations/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["conversations"]);
+    },
+  });
 
-  // const handleRead = (id) => {
-  //   mutation.mutate(id);
-  // };
+  const handleRead = (id) => {
+    mutation.mutate(id);
+  };
 
   return (
-    <section>
+    <section className="w-full">
       <Container>
-        <div className="messages">
+        <div className="flex">
         {isPending ? (
           "loading"
         ) : error ? (
           "error"
         ) : (
-          <div className="container">
-            <div className="title">
-              <h1>Messages</h1>
+          <div className="font-poppins py-12 w-full">
+            <div className="flex justify-between w-full">
+              <h1 className="font-semibold text-black text-opacity-85 text-xl">Messages</h1>
             </div>
-            <table>
-              <tr>
+            <table className="w-full">
+              <tr className="text-left h-24">
                 <th>{currentUser.user.isClient ? "Freelancer" : "Client"}</th>
                 <th>Last Message</th>
                 <th>Date</th>
@@ -56,25 +56,38 @@ const Messages = () => {
                   className={
                     ((currentUser.user.isClient && !c.readByClient) ||
                       (!currentUser.user.isClient && !c.readByFreelancer)) &&
-                    "active"
+                    "bg-green-700 bg-opacity-95"
                   }
                   key={c.id}
                 >
-                  <td>{currentUser.isSeller ? c.buyerId : c.sellerId}</td>
-                  <td>
+                  <td className="p-3">{currentUser.user.isClient ? c.freelancerId : c.clientId}</td>
+                  <td className="p-3">
                     <Link to={`/message/${c.id}`} className="link">
                       {c?.lastMessage?.substring(0, 100)}...
                     </Link>
                   </td>
                   <td>{moment(c.updatedAt).fromNow()}</td>
-                  <td>
-                    {((currentUser.isSeller && !c.readBySeller) ||
-                      (!currentUser.isSeller && !c.readByBuyer)) && (
-                      <button onClick={() => handleRead(c.id)}>
+                  {/* <td>
+                    {((currentUser.user.isClient && !c.readByClient) ||
+                      (!currentUser.user.isClient && c.readByFreelancer)) && (
+                      <Button className="text-white font-medium cursor-pointer bg-blue-500 p-3" onClick={() => handleRead(c.id)}>
                         Mark as Read
-                      </button>
+                      </Button>
                     )}
-                  </td>
+                    {
+                      console.log((!currentUser.user.isClient && !c.readByClient) || (currentUser.user.isClient && !c.readByFreelancer))
+                    }
+                    </td> */}
+                    <td>
+                  {((!currentUser.user.isClient && !c.readByClient) ||
+                    (currentUser.user.isClient && !c.readByFreelancer)) && (
+                      <Button className="text-white font-medium cursor-pointer bg-blue-500 p-3" onClick={() => handleRead(c.id)}>
+                      Mark as Read
+                    </Button>
+                  )}
+                </td>
+                    
+              
                 </tr>
               ))}
             </table>
