d15b65a324c7bf896a135a5147964b68445142c7
raunak mishra <raunakmishra6301@gmail.com>
2024-05-17 11:26:37 +0530

    gig page bug solved

--- src/components/Gig/MyGigs.jsx
+++ src/components/Gig/MyGigs.jsx
@@ -42,7 +42,7 @@ function MyGigs() {
                 <tr className='h-12 font-Roboto opacity-90'>
                     <th>Image</th>
                     <th>Title</th>
-                    <th>Price</th>
+                    <th>Price $</th>
                     <th>Sales</th>
                     <th>Action</th>
                 </tr>
@@ -56,7 +56,7 @@ function MyGigs() {
                     <td>{gig?.title}</td>
                     <td>{gig?.price}</td>
                     <td>{gig?.sales}</td>
-                    <td><i onClick={()=>handleDelete(gig?._id)} className="ri-delete-bin-6-line text-red-500"></i></td>
+                    <td><i onClick={()=>handleDelete(gig?._id)} className="cursor-pointer ri-delete-bin-6-line text-red-500"></i></td>
                 </tr>
                 ))
                }
